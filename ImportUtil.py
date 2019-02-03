0891f1e3555c7d700e2e7653f5d92b415f1e6fde
Roman <bytsa15@yandex.ru>
2019-02-03 07:36:01 +0000

    Import utils now work, make sure your data is in CITYSCAPES_DATASET env variable folder

--- ImportUtil.py
+++ ImportUtil.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, absolute_import, division
 import os, glob, sys
 from skimage.io import imread, imshow, imsave
-import cityscapescripts
+import cityscapesscripts
 def getData(num_tests, start):
     if 'CITYSCAPES_DATASET' in os.environ:
         cityscapesPath = os.environ['CITYSCAPES_DATASET']
@@ -9,20 +9,25 @@ def getData(num_tests, start):
         cityscapesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
     searchAnnotated = os.path.join(cityscapesPath, "gtFine", "train", "*", "*_gt*_labelTrain*")
     searchRaw = os.path.join(cityscapesPath, "img8bit", "train", "*", "*.png")
+
+    if not searchAnnotated:
+        printError("Did not find any annotated files.")
     filesAnnotated =glob.glob(searchAnnotated)
+
     filesRaw=glob.glob(searchRaw)
     filesAnnotated.sort()
     filesRaw.sort()
+
     """
     edit files so that filesAnnotated and filesRaw are the same shape and correspond to each other
     """
-    return (filesAnnotated[start:start+num_tests], filesRaw[start:start+num_tests])
+    return filesAnnotated[start:start+num_tests], filesRaw[start:start+num_tests]
 
 def importBatch(num_tests, start):   #load batch of data from train dataset
     y_files, X_files = getData(num_tests,start)
     X_input = []
     y_input = []
-    for i in range(X_files.shape[0]):
+    for i in range(len(y_files)):
         y_file = y_files[i]
         X_file = X_files[i]
         X_img = imread(X_file)
