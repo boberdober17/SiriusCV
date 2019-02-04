6a85a91b8e919a4740c12dc171e3d2e91d0dce50
Roman <bytsa15@yandex.ru>
2019-02-04 14:34:04 +0000

    Scaling in ImportUtil

--- ImportUtil.py
+++ ImportUtil.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, absolute_import, division
 import os, glob, sys
 from skimage.io import imread, imshow, imsave
+from skimage import data, color
+from skimage.transform import rescale, resize, downscale_local_mean
 import numpy as np
 import cityscapesscripts
 def getData(num_tests, start, type):
@@ -21,7 +23,7 @@ def getData(num_tests, start, type):
 
     return filesAnnotated[start:start+num_tests], filesRaw[start:start+num_tests]
 
-def importBatch(num_tests, start, verbose, type="train"):   #load batch of data from train dataset
+def importBatch(num_tests, start, verbose, type="train", scale = 0):   #load batch of data from train dataset
 
     y_files, X_files = getData(num_tests,start, type)
     X_input = []
@@ -39,9 +41,17 @@ def importBatch(num_tests, start, verbose, type="train"):   #load batch of data 
         X_file = X_files[i]
         filenames.append(X_file[:-16])
         X_img = imread(X_file)
+
+        if (scale != 0):
+            X_new = np.zeros((int(X_img.shape[0] / scale), int(X_img.shape[1] / scale),3))
+            k = 0
+            for x in X_img[::scale]:
+                X_new[k]=x[::scale]
+                k+=1
+                X_img = X_new
         X_input.append(X_img)
     z = 0
-    for i in range(len(X_files)):
+    for i in range(len(y_files)):
         z += 1
         if verbose:
             if z % 100 == 0:
@@ -49,12 +59,19 @@ def importBatch(num_tests, start, verbose, type="train"):   #load batch of data 
 
         y_file = y_files[i]
         y_img = imread(y_file)
+        if (scale != 0):
+            y_new = np.zeros((int(y_img.shape[0] / scale), int(y_img.shape[1] / scale)))
+            k = 0
+            for y in y_img[::scale]:
+                y_new[k] = y[::scale]
+                k += 1
+                y_img = y_new
         y_input.append(y_img)
 
 
     X = np.array(X_input)
     y = np.array(y_input)
-    if (type=='val')
+    if (type=='val'):
         return X,y, filenames
     return X, y
 def initTrain():
