c55c2a7c115217d36df05b2d248a84f64a36e0b1
Roman <bytsa15@yandex.ru>
2019-02-04 12:30:39 +0000

    changes idk =)

--- ImportUtil.py
+++ ImportUtil.py
@@ -3,49 +3,59 @@ import os, glob, sys
 from skimage.io import imread, imshow, imsave
 import numpy as np
 import cityscapesscripts
-def getData(num_tests, start):
+def getData(num_tests, start, type):
     if 'CITYSCAPES_DATASET' in os.environ:
         cityscapesPath = os.environ['CITYSCAPES_DATASET']
     else:
         cityscapesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
-    searchAnnotated = os.path.join(cityscapesPath, "gtFine", "train", "*", "*_gt*_labelTrain*")
-    searchRaw = os.path.join(cityscapesPath, "leftImg8bit", "train", "*", "*.png")
+    searchAnnotated = os.path.join(cityscapesPath, "gtFine", type, "*", "*_gt*_labelTrain*")
+    searchRaw = os.path.join(cityscapesPath, "leftImg8bit", type, "*", "*.png")
 
-    if not searchAnnotated:
-        printError("Did not find any annotated files.")
+    #if not searchAnnotated:
+    #    printError("Did not find any annotated files.")
     filesAnnotated =glob.glob(searchAnnotated)
 
     filesRaw=glob.glob(searchRaw)
     filesAnnotated.sort()
     filesRaw.sort()
 
-    """
-    edit files so that filesAnnotated and filesRaw are the same shape and correspond to each other
-    """
     return filesAnnotated[start:start+num_tests], filesRaw[start:start+num_tests]
 
-def importBatch(num_tests, start):   #load batch of data from train dataset
-    y_files, X_files = getData(num_tests,start)
+def importBatch(num_tests, start, verbose, type="train"):   #load batch of data from train dataset
+
+    y_files, X_files = getData(num_tests,start, type)
     X_input = []
     y_input = []
-    k = 0
+    if type=='val':
+        filenames = []
+    z = 0
     for i in range(len(X_files)):
 
-        y_file = y_files[i+k]
+        z+=1
+        if verbose:
+            if z % 100 == 0:
+                print('loaded files input - ', z)
+
         X_file = X_files[i]
-
-        while (y_file.split('/')[7][:-25] != X_file.split('/')[7][:-16]):
-            k+=1
-            y_file = y_files[i+k]
-
+        filenames.append(X_file[:-16])
         X_img = imread(X_file)
-        y_img = imread(y_file)
-
         X_input.append(X_img)
+    z = 0
+    for i in range(len(X_files)):
+        z += 1
+        if verbose:
+            if z % 100 == 0:
+               print('loaded files output - ', z)
+
+        y_file = y_files[i]
+        y_img = imread(y_file)
         y_input.append(y_img)
+
 
     X = np.array(X_input)
     y = np.array(y_input)
+    if (type=='val')
+        return X,y, filenames
     return X, y
 def initTrain():
     import cityscapesscripts.preparation.createTrainIdLabelImgs
